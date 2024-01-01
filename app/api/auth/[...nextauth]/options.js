358cc7f51ee6976c94ee00e9f0f8d5aae01f7821
szbence7 <bencusix@live.it>
2024-01-01 19:33:08 +0100

    message

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,88 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import User from "@/app/(models)/User";
+
+export const options = {
+    providers: [
+       GitHubProvider({
+        profile(profile) {
+            console.log("Profile Github", profile)
+
+            let userRole = 'GitHub User';
+            if(profile?.email == "bencusix@live.it") {
+                userRole = 'admin';
+            }
+
+            return {
+                ...profile,
+                role: userRole,
+            }
+        },
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+       }),
+       GoogleProvider({
+        profile(profile) {
+            console.log("Profile Google", profile)
+
+            let userRole = "Google User";
+
+            return {
+                ...profile,
+                id: profile.sub,
+                role: userRole,
+            }
+        },
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+       }),
+       CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+            email: {
+                label: "Email",
+                type: "text",
+                placeholder: "Enter your email address"
+            },
+            password: {
+                label: "Password",
+                type: "password",
+                placeholder: "Enter your password"
+            },
+        },
+        async authorize(credentials){
+            try {
+                const foundUser = await User.findOne({email: credentials.email}).lean().exec();
+
+                if (foundUser) {
+                    console.log("User Exists")
+                    const match = await bcrypt.compare(credentials.password, foundUser.password);
+
+                    if (match) {
+                        console.log("Good pass")
+                        delete foundUser.password
+
+                        foundUser["role"] = "Unverified email user"
+                        return foundUser
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+            }
+            return null;
+        }
+       }), 
+    ],
+    callbacks: {
+        async jwt({token, user}) {
+            if(user) token.role = user.role
+            return token
+        },
+        async session({session, token}) {
+            if(session?.user) session.user.role = token.role
+            return session
+        }
+    }
+};
